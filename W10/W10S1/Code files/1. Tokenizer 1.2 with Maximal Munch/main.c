43087818e0e695a5ee8f5d5f79eb6ff716e198cf
matthieudemari <matthieu_demari@sutd.edu.sg>
2023-03-23 20:07:41 +0800

    Reworked final tokenizer in W9S3, maximal munch in W10S1.

--- W10/W10S1/Code files/1. Tokenizer 1.2 with Maximal Munch/main.c
+++ W10/W10S1/Code files/1. Tokenizer 1.2 with Maximal Munch/main.c
@@ -4,7 +4,10 @@
 #include <stdbool.h>
 #include <regex.h>
 
-// Update the TokenType enumeration to include TOKEN_END_OF_LINE
+// Update the TokenType enumeration
+// We order tokens following the priority list we have defined
+// Keywords > Operators, Punctuations > Identifiers > Literals > Unknown
+// This will be important later on.
 typedef enum {
     TOKEN_KEYWORD_INT,
 	TOKEN_KEYWORD_FLOAT,
@@ -38,24 +41,22 @@ typedef enum {
 typedef struct {
     TokenType type;
     char *lexeme;
-	//int line_number;
+	int line_number;
 } Token;
 
 
 // Constructor for the Token struct
-//Token *create_token(TokenType type, const char *lexeme, int line_number) {
-Token *create_token(TokenType type, const char *lexeme) {
+Token *create_token(TokenType type, const char *lexeme, int line_number) {
     Token *token = (Token *)malloc(sizeof(Token));
     token->type = type;
     token->lexeme = strdup(lexeme);
-	//token->line_number = line_number;
+	token->line_number = line_number;
     return token;
 }
 
 
 // Destructor for the Token struct
 void free_token(Token *token) {
-	//free(token->line_number);
     free(token->lexeme);
     free(token);
 }
@@ -256,65 +257,86 @@ int main() {
         return 1;
     }
     */
-    char *source_code = strdup("int x = 7 float 2.5e-1 char 'a' char* \"hello\" ; \n"
-							   "while ( for ) if == return 1t0 + - * / & &&");
-    
-	// Splitting will have to be reworked to include line_tracking!
-	// (Missing feature at the moment)
-	//int line_number = 0;
-	char *lexeme = strtok(source_code, " \t\n\r");
-	
-	// Token stream definition
-	Token **token_stream = NULL;
+    char *source_code = strdup("int x =7 float 2.5e-1 char 'a' char* \"hello\"; \n"
+							   "while( for) if== return 1t0 +- @ * / & &&;");
+
+	// Create tokens stream
+    Token **token_stream = NULL;
     size_t token_count = 0;
 	
-	// Classify lexemes, has been reworked to include more token types
-    while (lexeme != NULL) {
-        TokenType token_type;
+	// Prepare maximal munch tracking variables
+    size_t source_code_length = strlen(source_code);
+    size_t current_position = 0;
+	int current_line_number = 1;
+	
+	// A possible maximal munch implementation
+    while (current_position < source_code_length) {
+		// Current lexeme candidate to be matched
+        TokenType token_type = TOKEN_UNKNOWN;
+        size_t longest_match = 0;
+        char matched_lexeme[256] = {0};
 		
-        // Now covering lots of possible token types!
-        // But to be honest there are many many more!
-         if (is_keyword_strcmp(lexeme) != TOKEN_UNKNOWN) {
-            token_type = is_keyword_strcmp(lexeme);
-        } else if (is_operator_strcmp(lexeme) != TOKEN_UNKNOWN) {
-            token_type = is_operator_strcmp(lexeme);
-        } else if (is_punctuation_strcmp(lexeme) != TOKEN_UNKNOWN) {
-            token_type = is_punctuation_strcmp(lexeme);
-        } else if (is_identifier_regex(lexeme)) {
-            token_type = TOKEN_IDENTIFIER;
-        } else if (is_integer_literal_regex(lexeme)) {
-            token_type = TOKEN_LITERAL_INT;
-        } else if (is_float_literal_regex(lexeme)) {
-            token_type = TOKEN_LITERAL_FLOAT;
-        } else if (is_char_literal_regex(lexeme)) {
-            token_type = TOKEN_LITERAL_CHAR;
-        } else if (is_string_literal_regex(lexeme)) {
-            token_type = TOKEN_LITERAL_STRING;
-        } else {
-            token_type = TOKEN_UNKNOWN;
-			// Error message
-			//printf("Error: Lexeme '%s' at line '%d' is UNKNOWN.\n", lexeme, line_number);
-			printf("Error: Lexeme '%s' is UNKNOWN.\n", lexeme);
+		// Left to right scan producing candidate lexemes and testing them
+        for (size_t i = current_position; i < source_code_length; ++i) {
+            char candidate_lexeme[256] = {0};
+            memcpy(candidate_lexeme, source_code + current_position, i - current_position + 1);
+            TokenType candidate_token_type = TOKEN_UNKNOWN;
+            size_t candidate_match_length = i - current_position + 1;
+			
+			// If at least one match on one of the possible token types,
+			// candidate_token_type will be the first token match found.
+			// (This is the reason why we have ordered our enum)
+			// Additional note: In the given if statement, we will evaluate each condition
+			// one by one using short-circuit evaluation. 
+			// As soon as one condition evaluates to true (i.e., the candidate_token_type is not TOKEN_UNKNOWN),
+			// the remaining conditions will not be evaluated, and the if block will execute.
+			// In this case, the candidate_token_type variable will have the value of the first
+			// "non-TOKEN_UNKNOWN" token type found during the evaluation of the conditions.
+			// The other conditions will not execute after that, and the code inside the if block will be executed
+			// using the found token_type and the corresponding matched_lexeme.
+            if ((candidate_token_type = is_keyword_strcmp(candidate_lexeme)) != TOKEN_UNKNOWN ||
+                (candidate_token_type = is_operator_strcmp(candidate_lexeme)) != TOKEN_UNKNOWN ||
+                (candidate_token_type = is_punctuation_strcmp(candidate_lexeme)) != TOKEN_UNKNOWN ||
+                (candidate_token_type = is_identifier_regex(candidate_lexeme) ? TOKEN_IDENTIFIER : TOKEN_UNKNOWN) != TOKEN_UNKNOWN ||
+                (candidate_token_type = is_integer_literal_regex(candidate_lexeme) ? TOKEN_LITERAL_INT : TOKEN_UNKNOWN) != TOKEN_UNKNOWN ||
+                (candidate_token_type = is_float_literal_regex(candidate_lexeme) ? TOKEN_LITERAL_FLOAT : TOKEN_UNKNOWN) != TOKEN_UNKNOWN ||
+                (candidate_token_type = is_char_literal_regex(candidate_lexeme) ? TOKEN_LITERAL_CHAR : TOKEN_UNKNOWN) != TOKEN_UNKNOWN ||
+                (candidate_token_type = is_string_literal_regex(candidate_lexeme) ? TOKEN_LITERAL_STRING : TOKEN_UNKNOWN) != TOKEN_UNKNOWN) {
+					token_type = candidate_token_type;
+					longest_match = candidate_match_length;
+					strcpy(matched_lexeme, candidate_lexeme);
+            }
         }
-
-        //Token *token = create_token(token_type, lexeme, line_number);
-		Token *token = create_token(token_type, lexeme);
 		
-		 // Resize the token stream and add the new token
-        token_stream = (Token **)realloc(token_stream, (token_count + 1) * sizeof(Token *));
-        token_stream[token_count] = token;
-        token_count++;
-        printf("Token { type: %d, lexeme: '%s' }\n", token->type, token->lexeme);
-        lexeme = strtok(NULL, " \t\n\r");
+		// After we have found our candidate lexeme, create token
+        if (token_type != TOKEN_UNKNOWN) {
+        Token *token = create_token(token_type, matched_lexeme, current_line_number);
+            token_stream = (Token **)realloc(token_stream, (token_count + 1) * sizeof(Token *));
+            token_stream[token_count] = token;
+            token_count++;
+            printf("Token { type: %d, lexeme: '%s', line: '%d'}\n", token->type, token->lexeme, token->line_number);
+            current_position += longest_match;
+        } else {
+			// Update the line number when encountering a newline character
+			if (source_code[current_position] == '\n') {
+				current_line_number++;
+			}
+			// Show error message, unless we have a whitespace character or some sort
+            if (source_code[current_position] != ' ' && source_code[current_position] != '\n' &&
+                source_code[current_position] != '\t' && source_code[current_position] != '\r') {
+                printf("Error: Unrecognized character '%c' at line %zu\n", source_code[current_position], current_position);
+            }
+            ++current_position;
+        }
     }
 	
-	// Free tokens in the token stream and free the token stream
+	// Free tokens stream
     for (size_t i = 0; i < token_count; i++) {
         free_token(token_stream[i]);
     }
     free(token_stream);
-
-	// Free source code string
+	
+	// Free source code
     free(source_code);
 
     return 0;
